def31db58c175f0378a6d4e40fc725cc7bed659f
rootniang <45300487+rootniang@users.noreply.github.com>
2022-04-19 22:18:39 +0000

    pagination

--- lecteur-flux-rss/src/app/app.component.ts
+++ lecteur-flux-rss/src/app/app.component.ts
@@ -8,7 +8,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
 
-  readonly url = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.lemonde.fr%2Frss%2Fen_continu.xml'
+  readonly url = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.lemonde.fr%2Frss%2Fen_continu.xml';
+  totalLenght : any ;
+  page : number = 1 ;
 
   feeds : any ;
   title! : string ;
@@ -18,6 +20,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.http.get(this.url).subscribe((data: any) =>{
       console.log("data", data.items) ;
+      this.totalLenght = data.items.lenght ;
       this.feeds = data.items ;
       this.title = data.feed.title ;
       this.description = data.feed.description ;
