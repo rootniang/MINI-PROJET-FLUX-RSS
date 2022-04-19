7d67de15753ffaa335d254f2fa196d79a5898731
rootniang <45300487+rootniang@users.noreply.github.com>
2022-04-19 19:53:21 +0000

    get flux rss as json

--- lecteur-flux-rss/src/app/app.component.ts
+++ lecteur-flux-rss/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'lecteur-flux-rss';
+export class AppComponent implements OnInit {
+
+  readonly url = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.lemonde.fr%2Frss%2Fen_continu.xml'
+
+  feeds : any ;
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get(this.url).subscribe((data: any) =>{
+      console.log("data", data.items) ;
+      this.feeds = data.items ;
+    })
+  }
+
 }
