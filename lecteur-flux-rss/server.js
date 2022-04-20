31bbb64a5a5fc96169b15ed2c5d8d4d6bee65609
rootniang <45300487+rootniang@users.noreply.github.com>
2022-04-20 00:06:24 +0000

    deploi

--- lecteur-flux-rss/server.js
+++ lecteur-flux-rss/server.js
@@ -5,7 +5,7 @@ const app = express() ;
 
 app.use(express.static(__dirname + '/dist/lecteur-flux-rss')) ;
 
-app.get("/*", function(res, req){
+app.get("/", function(res, req){
     res.sendFile(path.join(__dirname + '/dist/lecteur-flux-rss/index.html')) ;
 }) ;
 
